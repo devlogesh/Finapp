ff3d84c71668f1fe78c5e9ffd68db207dc6ea584
devlogesh <logeshtnind@gmail.com>
2024-03-10 12:32:10 +0530

    build files config

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+echo "building project...."
+python3.9 -m pip install -r requirements.txt
+
+
+echo "make migration..."
+python3.9 -m manage.py makemigrations --noinput
+python3.9 -m manage.py migrate --noinput
+
+echo "collect static...."
+python3.9 manage.py collectstatic --noinput --clear
