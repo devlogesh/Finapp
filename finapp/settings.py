34654257ed9744e4f188f9db907c3ff8e788488d
devlogesh <logeshtnind@gmail.com>
2024-03-10 14:10:43 +0530

    gitignore file

--- finapp/settings.py
+++ finapp/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'/'static'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/finappmodel/media/' 
